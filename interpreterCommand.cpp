46483f18d02d9cc567c843504201269711c6ed74
TomMCauser <TomMCauser@gmail.com>
2022-10-17 11:22:24 +0200

    Finish interpreterCommand, ready for debugging.

--- interpreterCommand.cpp
+++ interpreterCommand.cpp
@@ -16,6 +16,36 @@
 #include "txStatusSensor.h"
 #include "interpreterCommand.h"
 
-void interpreterCommand_int(){
-    
+static mode_t mode;
+
+void interpreterCommand_init(){
+	/*
+	interpreterCommand initially sets the transmission status to OFF
+	and sets the mode to SAFE
+	*/
+	sensor_setTxStatus(bool 0);
+	modes_set(mode_t SAFE);
+}
+
+void interpreterCommand_execute(command_t command){
+	/*
+	interpreterCommand_execute checks what the command is and then
+	executes it. If asked to turn transmission on while in SAFE mode,
+	the command will be ignored.
+	*/
+	if (command == TRANS_ON){
+		mode = modes_get();
+		if (mode!=SAFE){
+			sensor_setTxStatus(bool 1);
+		}
+	}
+        if (command == TRANS_OFF){
+		sensor_setTxStatus(bool 0);
+	}
+        if (command == NORMAL_MODE){
+                modes_set(mode_t NORMAL);
+        }
+        if (command == SAFE_MODE){
+                modes_set(mode_t SAFE);
+        }
 }
