fb9473a89a989753403e3c02bdde719219307da0
b1no <fhansch@stud.hs-bremen.de>
2022-10-20 17:45:09 +0200

    Clean and comment SamplerSensorP object.

--- samplerSensorP.cpp
+++ samplerSensorP.cpp
@@ -1,61 +1,57 @@
 /*
-  Authors: Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
-  Date: 2022-10-15
-  Version: 1.0
-  Language: C
+ * samplerSensorP.cpp
+ *
+ * Date: 2022-10-15
+ * Version: 1.0
+ * Language: C
+ *
+ * Title:
+ *		Sampler
+ *
+ * Method:
+ * 		The periodic task will run at an interval of SAMP_TIME. The wake time is
+ * 		set as an absolute time which will be incremented by SAMP_TIME on every
+ * 		run.
+ * 		The periodic task will check the mode and if the mode is SAFE it will set
+ * 		the transmission status OFF.
+ *
+ * Authors:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
+ *
+ * Reviewed:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis, 20 October 2022
+ *
+ * Note:
+ * 		In case the transmission status is set to OFF the sensor value will not
+ * 		be read because the voltage is neither transmitted nor logged. This has
+ * 		been discussed with the customer.
+ */
 
-  Responsiblities:
-
-  samplerSensorP is responsible for reading the voltage from AD-channel at a rate given by SAMP_TIME, and then forwarding this on for transmission.
-
-*/
-
- // Include Arduino library
 #include <Arduino.h>
-
- // Include the OS
 #include <Arduino_FreeRTOS.h>
-
- // Include local files
+#include "env_vars.h"
 #include "modes.h"
-#include "samplerSensorP.h"
 #include "txSensor.h"
 #include "txStatusSensor.h"
+#include "samplerSensorP.h"
 
+static void sampleLoop(void);
 
-static void sampleLoop(void *pvParameters); // Declaration for task
-static uint16_t sensorVoltage;
-static bool status;
-
-
-void samplerSensorP_activate(){
+void samplerSensorP_activate(void) {
   xTaskCreate(
     sampleLoop
     ,  "sampleLoop" // Name for identification
     ,  128  // The stack size
     ,  NULL
-    ,  1  // Priority. 3 is highest, 0 is lowest.
+    ,  1  /* TBD */
     ,  NULL );
 }
 
-static void sampleLoop(void *pvParameters){
-  (void) pvParameters;
-
-	uint16_t sensorValue; 
+static void sampleLoop(void) {
+	uint16_t sensorValue; /* sensor value read from ADC */
+	TickType_t xLastWakeTime; /* last tic function was run */
 
 	for (;;){
-		/*
-		This for-loop cycles infinitely. It does two mode checks. The first check is for SAFE mode,
-		which does nothing except continue to check the mode. The second check is for NORMAL mode,
-		which then does another check to see if we actually want to be transmitting. Provided that
-		we want to be transmitting, it will then finally use txSensor to transmit the value.
-		*/
-		/*
-		Provided that the mode is NORMAL, another check needs to be done to see if we are reading
-		the channel or not. If txStatusSensor returns a value of 0, then we do not call the
-		transmit function from txSensor.
-		*/
-
 		if (modes_get() == SAFE) {
 			txStatusSensor_set(0);
 		}
@@ -63,7 +59,8 @@ static void sampleLoop(void *pvParameters){
 			sensorValue = analogRead(ADC_PIN); 
 			txSensor_transmitAscii(sensorValue);
 		}
-		// TODO: @Anita This is the wrong tick type I think. Need to change to Tick_Type_t(?)
-		vTaskDelay( SAMP_TIME / portTICK_PERIOD_MS ); 
+		/* The SAMP_TIME is divided by the portTick_PERIOD_MS because SAMP_TIME
+		 * is specified in ms. */
+		vTaskDelayUntil(&xLastWakeTime, SAMP_TIME / portTICK_PERIOD_MS ); 
 	}
 }
--- samplerSensorP.h
+++ samplerSensorP.h
@@ -1,13 +1,31 @@
 /*
-  Authors: Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
-  Date: 2022-10-15
-  Version: 1.0
-  Language: C
+ * samplerSensorP.h
+ *
+ * Date: 2022-10-15
+ * Version: 1.0
+ * Language: C
+ *
+ * Title:
+ *		Sampler
+ *
+ * Responsibility:
+ *		This object is responsible to create a periodic task that is reading the 
+ *		voltage from AD-channel at a rate defined by SAMP_TIME, and	transmitting
+ *		the voltage.
+ *
+ * Public functions:
+ * 		activate():
+ * 			Creates and runs the periodic Task.
+ *
+ * Authors:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
+ *
+ * Reviewed:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis, 20 October 2022
+ */
+#ifndef SAMPLERSENSORP.H
+#define SAMPLERSENSORP.H
 
-  Responsiblities:
+void samplerSensorP_activate(void);
 
-  samplerSensorP is responsible for reading the voltage from AD-channel at a rate given by SAMP_TIME, and then forwarding this on for transmission.
-
-*/
-
-void samplerSensorP_activate();
+#endif
