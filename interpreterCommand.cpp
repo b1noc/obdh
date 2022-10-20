b50535d476a6fdcb048d2cdaedd4f4392228d5ed
b1no <fhansch@stud.hs-bremen.de>
2022-10-20 15:10:08 +0200

    Clean and Comment interpreter Command object.

--- interpreterCommand.cpp
+++ interpreterCommand.cpp
@@ -1,51 +1,56 @@
 /*
-  Authors: Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
-  Date: 2022-10-15
-  Version: 1.0
-  Language: C
-
-  Responsiblities:
-
-  interpreterCommand is responsible for interpreting and decoding commands.
-
-*/
-
+ * interpreterCommand.cpp
+ *
+ * Date: 2022-10-15
+ * Version: 1.0
+ * Language: C
+ *
+ * Title:
+ *		Interpreter Command 
+ *
+ * Method:
+ *		This object includes the logic given in [A1] for execution of commands.
+ *		It will set the mode and transmission status depending on the command 
+ *		received.
+ *		If the TRANS_ON signal is received it will check if the mode is SAFE 
+ *		before executing the command. If the mode is SAFE the command will be 
+ *		ignored.
+ *
+ * Authors:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
+ *
+ * Reviewed:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis, 20 October 2022
+ */
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <env_vars.h>
 #include "modes.h"
-#include "interpreterCommand.h"
 #include "sensor.h"
+#include "interpreterCommand.h"
 
-static mode_t mode;
-
-void interpreterCommand_init(){
-	/*
-	interpreterCommand initially sets the transmission status to OFF
-	and sets the mode to SAFE
-	*/
-	//TODO: check if they should really be removed
-	//sensor_setTxStatus(0);
-	//modes_set(SAFE);
+void interpreterCommand_init(void) {
+	/* empty */
 }
 
-void interpreterCommand_execute(command_t command){
-	/*
-	interpreterCommand_execute checks what the command is and then
-	executes it. If asked to turn transmission on while in SAFE mode,
-	the command will be ignored.
-	*/
-	if (command == SAFE_MODE){
+void interpreterCommand_execute(command_t command) {
+
+#ifdef DEBUG
+	Serial.println("Command execute = " + (String) command);
+#endif
+
+	if (command == SAFE_MODE) {
 		modes_set(SAFE);
 	}
-	else if (command == NORMAL_MODE){
+	else if (command == NORMAL_MODE) {
 		modes_set(NORMAL);
 	}
-	else if (command == TRANS_ON){
+	else if (command == TRANS_ON) {
 		if (modes_get() != SAFE){
 			sensor_setTxStatus(1);
 		}
 	}
-	else if (command == TRANS_OFF){
+	else if (command == TRANS_OFF) {
 		sensor_setTxStatus(0);
 	}
 }
--- interpreterCommand.h
+++ interpreterCommand.h
@@ -1,17 +1,35 @@
 /*
-  Authors: Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
-  Date: 2022-10-15
-  Version: 1.0
-  Language: C
+ * interpreterCommand.h
+ *
+ * Date: 2022-10-15
+ * Version: 1.0
+ * Language: C
+ *
+ * Title:
+ *		Interpreter Command 
+ *
+ * Responsibility:
+ *		This object is responsible for interpreting the received command and
+ *		taking appropriate action according to [A1].
+ *		It can perform mode changes and change the transmission status.
+ *
+ * Public functions:
+ * 		init():
+ * 			Initialize the object.
+ *
+ * 		execute(command_t command):
+ * 			Executes the command from the input parameter command.
+ *
+ * Authors:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
+ *
+ * Reviewed:
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis, 20 October 2022
+ */
+#ifndef INTERPRETERCOMMAND.H
+#define INTERPRETERCOMMAND.H
 
-  Responsiblities:
-
-  interpreterCommand is responsible for interpreting and decoding commands.
-
-*/
-
-
-#include "env_vars.h"
-
-void interpreterCommand_init();
+void interpreterCommand_init(void);
 void interpreterCommand_execute(command_t command);
+
+#endif
