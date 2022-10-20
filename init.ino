01455d6cd62a93d842c540a29175eadf0bf0b920
b1no <fhansch@stud.hs-bremen.de>
2022-10-20 15:56:12 +0200

    Clean and comment init - again.

--- init.ino
+++ init.ino
@@ -1,26 +1,27 @@
-/* 
- * init.ino 
+/*
+ * init.ino
  *
  * Date: 2022-10-15
  * Version: 1.0
  * Language: C
  *
  * Title:
- *    Init Object
+ *		Init
  *
- * Responsibility:
- *    This object is responsible for initializing all first level objects.
+ * Method:
+ *		This object is responsible for initializing all first level objects.
  *
  * Authors:
  * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis
  *
  * Reviewed:
- * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis, 19 October 2022
+ * 		Tom Causer, Finn Hansch, Jacek Patora, Pavlos Vlazakis, 20 October 2022
  *
- * NOTE:
- *    This object has no header file as it is the initial file for the Arduino compiler. 
-*/
-
+ * Note:
+ *		This object has no header file as it is the initial file for the
+ *		Arduino compiler. 
+ */
+#include <Arduino_FreeRTOS.h> // only required according to Appendix C
 #include "env_vars.h"
 #include "sensor.h"
 #include "modes.h"
@@ -43,4 +44,6 @@ void setup() {
 #endif
 }
 
-void loop() {}
+void loop() {
+	/* empty */
+}
